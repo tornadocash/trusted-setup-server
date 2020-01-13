7220aac5ce203ee7ae99499e1dee06e0564cc333
poma <semenov.roma@gmail.com>
2020-01-13 18:54:16 +0700

    Simple UI and button, docker-compose

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,32 @@
-import init, { contribute } from './pkg/phase2.js';
+import init, { contribute } from './pkg/phase2.js'
+let initPromise = init()
+document.getElementById('contributeBtn').onclick = makeContribution
 
-async function main() {
-    await init();
+const timeout = ms => new Promise(res => setTimeout(res, ms))
 
-    console.log('Downloading challenge')
+function status(text) {
+    document.getElementById('status').innerHTML = text
+    console.log(text)
+}
+
+async function makeContribution() {
+    await initPromise
+
+    status('Downloading last contribution')
     let data = await fetch('challenge')
     data = await data.arrayBuffer()
     data = new Uint8Array(data)
+    status('Generating random contribution')
+    await timeout(100) // allow UI to update before freezing in wasm
     console.log('Source params', data)
     let result = contribute(data)
     console.log('Updated params', result)
-    console.log('Uploading response')
-    const formData  = new FormData();
-    formData.append("response", new Blob([result], { type:"application/octet-stream" }));
+    status('Uploading and verifying your contribution')
+    const formData  = new FormData()
+    formData.append("response", new Blob([result], { type:"application/octet-stream" }))
     await fetch('response', {
         method: 'POST',
         body: formData
     })
-    console.log('Done')
+    status('Your contribution is verified and recorded. THX BYE.')
 }
-
-main().catch(console.error);
