b6702a0722c0611b127553fcf8502b9dfa45cc63
Danil Kovtonyuk <danx.kov@gmail.com>
2020-01-29 23:13:06 +1000

    tornado styles

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-navbar wrapper-class="container" class="header">
+    <template slot="brand">
+      <b-navbar-item :to="{ path: '/' }" tag="router-link">
+        <Logo />
+      </b-navbar-item>
+    </template>
+    <template slot="end">
+      <b-navbar-item href="#">
+        Instructions
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+import Logo from '@/components/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,26 +4,71 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Tornado Trusted Setup Ceremony',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+      { name: 'theme-color', content: '#000403' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'Non-custodial Ethereum Privacy solution.'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Tornado Trusted Setup Ceremony'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Non-custodial, trustless, serverless, private transactions on Ethereum network'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://ceremony.tornado.cash'
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://tornado.cash/tw.png'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Non-custodial, trustless, serverless, private transactions on Ethereum network'
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content:
+          'Mixer, Ethereum, ERC20, dapp, smart contract, decentralized, metamask, zksnark, zero knowledge'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'manifest', href: 'manifest.json' },
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+      { rel: 'apple-touch-icon', href: '/favicon/apple-touch-icon.png' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=PT+Mono&display=swap'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#94febf', height: '5px', duration: 5000 },
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['@/assets/styles/app.scss'],
   /*
    ** Plugins to load before mounting the App
    */
@@ -40,7 +85,13 @@ module.exports = {
    */
   modules: [
     // Doc: https://buefy.github.io/#/documentation
-    'nuxt-buefy',
+    [
+      'nuxt-buefy',
+      {
+        css: false,
+        materialDesignIcons: false
+      }
+    ],
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios'
   ],
@@ -56,6 +107,12 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {},
+    html: {
+      minify: {
+        collapseWhitespace: true, // as @dario30186 mentioned
+        removeComments: true // 👈 add this line
+      }
+    }
   }
 }
