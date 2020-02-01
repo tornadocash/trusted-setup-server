213a7879fde443faa91e098e1e39c579e854617b
Danil Kovtonyuk <danx.kov@gmail.com>
2020-02-02 03:17:32 +1000

    add table layout
    -change route to make contribution

--- pages/index.vue
+++ pages/index.vue
@@ -1,159 +1,167 @@
 <template>
   <div class="ceremony">
-    <div class="title is-size-1 is-spaced">
-      Hello, <span>@{{ user.handle }}</span>
-    </div>
-    <div class="subtitle">Lorem ipsum dolor sit amet, consectetur?</div>
-    <p class="p">
-      If you don’t trust binaries, we encorage you to follow this <a href="">instruction</a> to
-      contribute by compiling from source code. It is very easy!
+    <h1 class="title is-size-1 is-spaced">Lorem <span>Ipsum Dolor</span></h1>
+    <p class="p is-size-5">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
     </p>
-
-    <div class="columns is-centered">
-      <div class="column is-one-third">
-        <div class="box">
-          <div class="title is-5">Lorem ipsum</div>
-          <Cloak />
-        </div>
-      </div>
-      <div class="column is-one-third">
-        <div :class="{ 'is-hovered': isLoggedIn }" class="box">
-          <div class="title is-5">Lorem ipsum</div>
-          <div v-if="isLoggedIn" class="fields">
-            <b-field label="Name">
-              <b-input v-model="user.name"></b-input>
-            </b-field>
-            <b-field label="Company">
-              <b-input v-model="user.company"></b-input>
-            </b-field>
-          </div>
-          <div v-else class="buttons">
-            <b-button @click="logIn" type="is-primary" outlined expanded>
-              Sign In
-            </b-button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div v-show="status.msg !== ''" class="status">
-      <div :class="status.type" class="status-message">{{ status.msg }}</div>
-      <div v-show="status.type === ''" class="status-spinner"></div>
-    </div>
-
     <div class="buttons is-centered">
-      <b-button
-        v-if="!isContributeBtnDisabled"
-        @click="makeContribution"
-        type="is-primary"
-        outlined
-      >
+      <b-button type="is-primary" outlined tag="router-link" to="/make-contribution">
         Make the contribution
       </b-button>
-      <b-button
-        v-if="isContributeBtnDisabled && status.type === 'is-success'"
-        type="is-primary"
-        tag="a"
-        href="https://twitter.com/intent/tweet?text=Hello%20world"
-        target="_blank"
-        outlined
-      >
-        Tweet about your contribution
-      </b-button>
     </div>
+    <div class="currently">Currently there are <span>8999</span> contributions</div>
+
+    <b-table
+      :data="filteredContributions"
+      :hoverable="true"
+      :mobile-cards="false"
+      :per-page="rowsPerPage"
+      paginated
+      pagination-position="both"
+    >
+      <template slot-scope="props">
+        <b-table-column field="id" label="#" width="40" numeric>
+          {{ props.row.id }}
+        </b-table-column>
+
+        <b-table-column label="Account">
+          <a :href="`#${props.row.account}`" target="_blank">{{ props.row.account }}</a>
+        </b-table-column>
+
+        <b-table-column field="name" label="Name">
+          {{ props.row.name }}
+        </b-table-column>
+
+        <b-table-column field="company" label="Company">
+          {{ props.row.company }}
+        </b-table-column>
+
+        <b-table-column label="Attestation">
+          <a :href="props.row.attestation" target="_blank">{{ props.row.account }}</a>
+        </b-table-column>
+
+        <b-table-column>
+          <a :href="props.row.contribution" class="button is-icon">
+            <Link />
+          </a>
+        </b-table-column>
+      </template>
+
+      <template slot="empty">
+        <section class="section">
+          <div class="content has-text-centered">
+            <p>
+              <span class="icon icon-emoticon-sad icon-48px"></span>
+            </p>
+            <p>Nothing here.</p>
+          </div>
+        </section>
+      </template>
+
+      <template slot="top-left">
+        <b-field class="table-search">
+          <b-input
+            v-model="contributionSearch"
+            placeholder="Search..."
+            type="search"
+            icon="magnify"
+          ></b-input>
+        </b-field>
+      </template>
+
+      <template slot="bottom-left">
+        <b-field horizontal label="Show">
+          <b-dropdown v-model="rowsPerPage" expanded aria-role="list" position="is-top-right">
+            <div slot="trigger" class="control">
+              <div class="input">
+                <span>{{ rowsPerPage }}</span>
+              </div>
+            </div>
+            <b-dropdown-item
+              v-for="(rows, index) in [10, 15, 20, 50]"
+              :key="index"
+              :value="rows"
+              aria-role="listitem"
+            >
+              {{ rows }}
+            </b-dropdown-item>
+          </b-dropdown>
+        </b-field>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
-/* eslint-disable no-console */
-import Cloak from '@/components/Cloak'
-const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+import Link from '@/components/icons/Link'
 
 export default {
   components: {
-    Cloak
+    Link
   },
   data() {
     return {
-      isContributeBtnDisabled: false,
-      status: {
-        type: '',
-        msg: ''
-      },
-      user: { name: '', handle: 'Anonymous', company: '' }
+      contributions: [
+        {
+          id: 1,
+          account: '@VitalikButerin',
+          name: 'Vitalik Buterin',
+          company: 'Ethereum',
+          attestation: 'https://twitter.com/VitalikButerin/status/1220158987456237568',
+          contribution: '#'
+        },
+        {
+          id: 2,
+          account: '@Chims1974',
+          name: 'Rickey Kline',
+          company: 'Big Wheel',
+          attestation: '#',
+          contribution: '#'
+        },
+        {
+          id: 3,
+          account: '@Diguest',
+          name: 'Ryan Obrien',
+          company: 'Brilliant Home',
+          attestation: '#',
+          contribution: '#'
+        },
+        {
+          id: 4,
+          account: '@Mathervenrat',
+          name: 'William Hartwig',
+          company: 'New World',
+          attestation: '#',
+          contribution: '#'
+        },
+        {
+          id: 5,
+          account: '@Hichercy',
+          name: 'Wayne Biggins',
+          company: 'Balanced Fortune',
+          attestation: '#',
+          contribution: '#'
+        },
+        {
+          id: 6,
+          account: '',
+          name: 'Anonymous',
+          company: '',
+          attestation: '',
+          contribution: '#'
+        }
+      ],
+      rowsPerPage: 10,
+      contributionSearch: ''
     }
   },
   computed: {
-    isLoggedIn() {
-      return !!this.user.name && this.user.name !== 'Anonymous'
-    }
-  },
-  async mounted() {
-    try {
-      const data = await this.$axios.$get('/api/user_data')
-      console.log('data', data)
-      if (data.name !== 'Anonymous') {
-        this.user.handle = data.handle
-        this.user.name = data.name
-      }
-    } catch (e) {
-      console.error('user_data fail', e)
-    }
-  },
-  methods: {
-    async makeContribution({ retry = 0 } = {}) {
-      try {
-        this.isContributeBtnDisabled = true
-
-        this.status.msg = 'Downloading last contribution'
-        this.status.type = ''
-        let data = await fetch('api/challenge')
-        data = new Uint8Array(await data.arrayBuffer())
-
-        this.status.msg = 'Generating random contribution'
-        await timeout(100) // allow UI to update before freezing in wasm
-        console.log('Source params', data)
-        const contribute = await this.$contribute()
-        const result = contribute(data)
-        console.log('Updated params', result)
-
-        this.status.msg = 'Uploading and verifying your contribution'
-        console.log('this.user.name', this.user)
-        const formData = new FormData()
-        formData.append('response', new Blob([result], { type: 'application/octet-stream' }))
-        formData.append('name', this.user.name)
-        formData.append('company', this.user.company)
-        const resp = await fetch('api/response', {
-          method: 'POST',
-          body: formData
-        })
-
-        if (resp.ok) {
-          this.status.msg = 'Your contribution is verified and recorded. THX BYE.'
-          this.status.type = 'is-success'
-        } else if (resp.status === 422) {
-          if (retry < 3) {
-            console.log(`Looks like someone else uploaded contribution ahead of us, retrying`)
-            await this.makeContribution({ retry: retry++ })
-          } else {
-            this.status.msg = `Failed to upload your contribution after ${retry} attempts`
-            this.status.type = 'is-danger'
-            this.isContributeBtnDisabled = false
-          }
-        } else {
-          this.status.msg = 'Error uploading your contribution'
-          this.status.type = 'is-danger'
-          this.isContributeBtnDisabled = false
-        }
-      } catch (e) {
-        console.error(e.message)
-        this.status.msg = e.message
-        this.status.type = 'is-danger'
-        this.isContributeBtnDisabled = false
-      }
-    },
-    logIn() {
-      window.location.replace('/api/connect')
+    filteredContributions() {
+      return this.contributions.filter((contribution) => {
+        return contribution.name.toLowerCase().includes(this.contributionSearch.toLowerCase())
+      })
     }
   }
 }
---
+++ pages/make-contribution.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="ceremony">
+    <h1 class="title is-size-1 is-spaced">
+      Hello, <span>@{{ user.handle }}</span>
+    </h1>
+    <h2 class="subtitle">Lorem ipsum dolor sit amet, consectetur?</h2>
+    <p class="p">
+      If you don’t trust binaries, we encorage you to follow this <a href="">instruction</a> to
+      contribute by compiling from source code. It is very easy!
+    </p>
+
+    <div class="columns is-centered">
+      <div class="column is-one-third">
+        <div class="box">
+          <div class="title is-5">Lorem ipsum</div>
+          <Cloak />
+        </div>
+      </div>
+      <div class="column is-one-third">
+        <div :class="{ 'is-hovered': isLoggedIn }" class="box">
+          <div class="title is-5">Lorem ipsum</div>
+          <div v-if="isLoggedIn" class="fields">
+            <b-field label="Name">
+              <b-input v-model="user.name"></b-input>
+            </b-field>
+            <b-field label="Company">
+              <b-input v-model="user.company"></b-input>
+            </b-field>
+          </div>
+          <div v-else class="buttons">
+            <b-button @click="logIn" type="is-primary" outlined expanded>
+              Sign In
+            </b-button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-show="status.msg !== ''" class="status">
+      <div :class="status.type" class="status-message">{{ status.msg }}</div>
+      <div v-show="status.type === ''" class="status-spinner"></div>
+    </div>
+
+    <div class="buttons is-centered">
+      <b-button
+        v-if="!isContributeBtnDisabled"
+        @click="makeContribution"
+        type="is-primary"
+        outlined
+      >
+        Make the contribution
+      </b-button>
+      <b-button
+        v-if="isContributeBtnDisabled && status.type === 'is-success'"
+        type="is-primary"
+        tag="a"
+        href="https://twitter.com/intent/tweet?text=Hello%20world"
+        target="_blank"
+        outlined
+      >
+        Tweet about your contribution
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+import Cloak from '@/components/Cloak'
+const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export default {
+  components: {
+    Cloak
+  },
+  data() {
+    return {
+      isContributeBtnDisabled: false,
+      status: {
+        type: '',
+        msg: ''
+      },
+      user: { name: '', handle: 'Anonymous', company: '' }
+    }
+  },
+  computed: {
+    isLoggedIn() {
+      return !!this.user.name && this.user.name !== 'Anonymous'
+    }
+  },
+  async mounted() {
+    try {
+      const data = await this.$axios.$get('/api/user_data')
+      console.log('data', data)
+      if (data.name !== 'Anonymous') {
+        this.user.handle = data.handle
+        this.user.name = data.name
+      }
+    } catch (e) {
+      console.error('user_data fail', e)
+    }
+  },
+  methods: {
+    async makeContribution({ retry = 0 } = {}) {
+      try {
+        this.isContributeBtnDisabled = true
+
+        this.status.msg = 'Downloading last contribution'
+        this.status.type = ''
+        let data = await fetch('api/challenge')
+        data = new Uint8Array(await data.arrayBuffer())
+
+        this.status.msg = 'Generating random contribution'
+        await timeout(100) // allow UI to update before freezing in wasm
+        console.log('Source params', data)
+        const contribute = await this.$contribute()
+        const result = contribute(data)
+        console.log('Updated params', result)
+
+        this.status.msg = 'Uploading and verifying your contribution'
+        console.log('this.user.name', this.user)
+        const formData = new FormData()
+        formData.append('response', new Blob([result], { type: 'application/octet-stream' }))
+        formData.append('name', this.user.name)
+        formData.append('company', this.user.company)
+        const resp = await fetch('api/response', {
+          method: 'POST',
+          body: formData
+        })
+
+        if (resp.ok) {
+          this.status.msg = 'Your contribution is verified and recorded. THX BYE.'
+          this.status.type = 'is-success'
+        } else if (resp.status === 422) {
+          if (retry < 3) {
+            console.log(`Looks like someone else uploaded contribution ahead of us, retrying`)
+            await this.makeContribution({ retry: retry++ })
+          } else {
+            this.status.msg = `Failed to upload your contribution after ${retry} attempts`
+            this.status.type = 'is-danger'
+            this.isContributeBtnDisabled = false
+          }
+        } else {
+          this.status.msg = 'Error uploading your contribution'
+          this.status.type = 'is-danger'
+          this.isContributeBtnDisabled = false
+        }
+      } catch (e) {
+        console.error(e.message)
+        this.status.msg = e.message
+        this.status.type = 'is-danger'
+        this.isContributeBtnDisabled = false
+      }
+    },
+    logIn() {
+      window.location.replace('/api/connect')
+    }
+  }
+}
+</script>
