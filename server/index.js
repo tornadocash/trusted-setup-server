f2bb2eaf6e76c9f6156e69bd39dab6811facb433
Alexey <peretsmobil@gmail.com>
2020-03-02 12:09:41 +0300

    fix deploying process

--- server/index.js
+++ server/index.js
@@ -14,19 +14,13 @@ const attestationWatcher = require('./attestationWatcher')
 const app = express()
 
 async function start() {
-  config.dev = NODE_ENV !== 'production'
   await models.sequelize.sync()
   const nuxt = new Nuxt(config)
-
   const { host, port } = nuxt.options.server
 
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  } else {
-    await nuxt.ready()
-  }
+  const builder = new Builder(nuxt)
+  await builder.build()
+
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
