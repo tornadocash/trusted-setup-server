db9ee0c89172747a5a81cd4b95b164f2a2ee4751
benber86 <benoitb@tutanota.de>
2021-05-19 10:35:52 +1000

    Add Japanese locale

--- langs/index.js
+++ langs/index.js
@@ -2,10 +2,12 @@ import en from './en.json'
 import kr from './kr.json'
 import de from './de.json'
 import fr from './fr.json'
+import jp from './jp.json'
 
 export default {
   en,
   kr,
   de,
-  fr
+  fr,
+  jp
 }
